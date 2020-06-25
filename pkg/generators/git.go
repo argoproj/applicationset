e82fd8dd093827f825247d6b27368096b2844244
Omer Kahani <kahaniomer@gmail.com>
2020-06-25 11:54:57 +0300

    add git directory crd

---
+++ pkg/generators/git.go
@@ -0,0 +1,25 @@
+package generators
+
+import (
+	"fmt"
+	argoprojiov1alpha1 "github.com/argoproj-labs/applicationset/api/v1alpha1"
+	argov1alpha1 "github.com/argoproj/argo-cd/pkg/apis/application/v1alpha1"
+)
+
+var _ Generator = (*GitGenerator)(nil)
+
+type GitGenerator struct {
+}
+
+func NewGitGenerator() Generator {
+	g := &GitGenerator{}
+	return g
+}
+
+func (g *GitGenerator) GenerateApplications(appSet *argoprojiov1alpha1.ApplicationSet) ([]argov1alpha1.Application, error) {
+	if appSet == nil {
+		return nil, fmt.Errorf("ApplicationSet is empty")
+	}
+
+	return nil, nil
+}
