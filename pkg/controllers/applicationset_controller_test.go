6375dcb202cdab8117096da479f2c4e4f2137a61
Omer Kahani <kahaniomer@gmail.com>
2020-07-07 23:31:23 +0300

    refactor

--- pkg/controllers/applicationset_controller_test.go
+++ pkg/controllers/applicationset_controller_test.go
@@ -7,7 +7,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	crtclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"testing"
 
 	argoprojiov1alpha1 "github.com/argoproj-labs/applicationset/api/v1alpha1"
@@ -20,51 +20,61 @@ func TestCreateApplications(t *testing.T) {
 	scheme := runtime.NewScheme()
 	argov1alpha1.AddToScheme(scheme)
 
-	c := fake.NewFakeClientWithScheme(scheme)
+	client := fake.NewFakeClientWithScheme(scheme)
 
 	r := ApplicationSetReconciler{
-		Client: c,
+		Client: client,
 		Scheme: scheme,
 		Recorder: record.NewFakeRecorder(1),
 	}
 
-	appSet := argoprojiov1alpha1.ApplicationSet{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:                       "name",
-			Namespace:                  "namespace",
-		},
-	}
-
-	apps := []argov1alpha1.Application{
+	for _, c := range []struct {
+		appSet argoprojiov1alpha1.ApplicationSet
+		apps []argov1alpha1.Application
+		expected []argov1alpha1.Application
+	}{
 		{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:                       "app1",
+			appSet: argoprojiov1alpha1.ApplicationSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:                       "name",
+					Namespace:                  "namespace",
+				},
+			},
+			apps: []argov1alpha1.Application{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:                       "app1",
+					},
+				},
+			},
+			expected: []argov1alpha1.Application{
+				{
+					TypeMeta: metav1.TypeMeta{
+						Kind:       "Application",
+						APIVersion: "argoproj.io/v1alpha1",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "app1",
+						Namespace:       "namespace",
+						ResourceVersion: "1",
+					},
+				},
 			},
 		},
+	}{
+		r.createApplications(context.TODO(), c.appSet, c.apps)
+
+		for _, e := range c.expected {
+			got := &argov1alpha1.Application{}
+			_ = client.Get(context.Background(), crtclient.ObjectKey{
+				Namespace: e.Namespace,
+				Name:      e.Name,
+			}, got)
+
+			assert.Equal(t, e, *got)
+		}
 	}
 
-	r.createApplications(context.TODO(), appSet, apps)
 
-
-	got := &argov1alpha1.Application{}
-	_ = c.Get(context.Background(), client.ObjectKey{
-		Namespace: "namespace",
-		Name:      "app1",
-	}, got)
-
-
-	expected := argov1alpha1.Application{
-		TypeMeta:   metav1.TypeMeta{
-			Kind:       "Application",
-			APIVersion: "argoproj.io/v1alpha1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:                       "app1",
-			Namespace:                  "namespace",
-			ResourceVersion:			"1",
-		},
-	}
-
-	assert.Equal(t, expected, *got)
 
 }
