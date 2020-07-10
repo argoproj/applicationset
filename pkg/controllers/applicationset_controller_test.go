d9e624b8c54ccf5e17cccb992393332d08632567
Omer Kahani <8766193+OmerKahani@users.noreply.github.com>
2020-07-10 04:58:11 +0300

    add update and owner reference (#10)

--- pkg/controllers/applicationset_controller_test.go
+++ pkg/controllers/applicationset_controller_test.go
@@ -8,30 +8,24 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 	crtclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"testing"
 
 	argoprojiov1alpha1 "github.com/argoproj-labs/applicationset/api/v1alpha1"
-
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
 func TestCreateApplications(t *testing.T) {
 
 	scheme := runtime.NewScheme()
+	argoprojiov1alpha1.AddToScheme(scheme)
 	argov1alpha1.AddToScheme(scheme)
 
-	client := fake.NewFakeClientWithScheme(scheme)
-
-	r := ApplicationSetReconciler{
-		Client:   client,
-		Scheme:   scheme,
-		Recorder: record.NewFakeRecorder(1),
-	}
-
 	for _, c := range []struct {
-		appSet   argoprojiov1alpha1.ApplicationSet
-		apps     []argov1alpha1.Application
-		expected []argov1alpha1.Application
+		appSet     argoprojiov1alpha1.ApplicationSet
+		existsApps []argov1alpha1.Application
+		apps       []argov1alpha1.Application
+		expected   []argov1alpha1.Application
 	}{
 		{
 			appSet: argoprojiov1alpha1.ApplicationSet{
@@ -40,6 +34,7 @@ func TestCreateApplications(t *testing.T) {
 					Namespace: "namespace",
 				},
 			},
+			existsApps: nil,
 			apps: []argov1alpha1.Application{
 				{
 					ObjectMeta: metav1.ObjectMeta{
@@ -61,17 +56,90 @@ func TestCreateApplications(t *testing.T) {
 				},
 			},
 		},
+		{
+			appSet: argoprojiov1alpha1.ApplicationSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "name",
+					Namespace: "namespace",
+				},
+				Spec: argoprojiov1alpha1.ApplicationSetSpec{
+					Template: argoprojiov1alpha1.ApplicationSetTemplate{
+						Spec: argov1alpha1.ApplicationSpec{
+							Project: "project",
+						},
+					},
+				},
+			},
+			existsApps: []argov1alpha1.Application{
+				argov1alpha1.Application{
+					TypeMeta: metav1.TypeMeta{
+						Kind:       "Application",
+						APIVersion: "argoproj.io/v1alpha1",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "app1",
+						Namespace:       "namespace",
+						ResourceVersion: "2",
+					},
+					Spec: argov1alpha1.ApplicationSpec{
+						Project: "test",
+					},
+				},
+			},
+			apps: []argov1alpha1.Application{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "app1",
+					},
+					Spec: argov1alpha1.ApplicationSpec{
+						Project: "project",
+					},
+				},
+			},
+			expected: []argov1alpha1.Application{
+				{
+					TypeMeta: metav1.TypeMeta{
+						Kind:       "Application",
+						APIVersion: "argoproj.io/v1alpha1",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:            "app1",
+						Namespace:       "namespace",
+						ResourceVersion: "3",
+					},
+					Spec: argov1alpha1.ApplicationSpec{
+						Project: "project",
+					},
+				},
+			},
+		},
 	} {
+		initObjs := []runtime.Object{&c.appSet}
+		for _, a := range c.existsApps {
+			controllerutil.SetControllerReference(&c.appSet, &a, scheme)
+			initObjs = append(initObjs, &a)
+		}
+
+		client := fake.NewFakeClientWithScheme(scheme, initObjs...)
+
+		r := ApplicationSetReconciler{
+			Client:   client,
+			Scheme:   scheme,
+			Recorder: record.NewFakeRecorder(1),
+		}
+
 		r.createApplications(context.TODO(), c.appSet, c.apps)
 
-		for _, e := range c.expected {
+		for _, obj := range c.expected {
 			got := &argov1alpha1.Application{}
 			_ = client.Get(context.Background(), crtclient.ObjectKey{
-				Namespace: e.Namespace,
-				Name:      e.Name,
+				Namespace: obj.Namespace,
+				Name:      obj.Name,
 			}, got)
 
-			assert.Equal(t, e, *got)
+			controllerutil.SetControllerReference(&c.appSet, &obj, r.Scheme)
+
+			assert.Equal(t, obj, *got)
 		}
 	}
 
