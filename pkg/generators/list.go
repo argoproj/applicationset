cc596a4ce2cd3ed394520101fc8c082b11c49718
Matteo Ruina <matteo.ruina@gmail.com>
2021-01-29 09:07:06 +0100

    Add support for support arbitrary key:value pairs in ListGenerator
    Update install
    
    Update to 0.3.0

--- pkg/generators/list.go
+++ pkg/generators/list.go
@@ -1,6 +1,7 @@
 package generators
 
 import (
+	"fmt"
 	"time"
 
 	argoprojiov1alpha1 "github.com/argoproj-labs/applicationset/api/v1alpha1"
@@ -37,9 +38,12 @@ func (g *ListGenerator) GenerateParams(appSetGenerator *argoprojiov1alpha1.Appli
 	res := make([]map[string]string, len(appSetGenerator.List.Elements))
 
 	for i, tmpItem := range appSetGenerator.List.Elements {
-		params := make(map[string]string, 2)
+		params := make(map[string]string, 2+len(tmpItem.Values))
 		params[utils.ClusterListGeneratorKeyName] = tmpItem.Cluster
 		params[utils.UrlGeneratorKeyName] = tmpItem.Url
+		for key, value := range tmpItem.Values {
+			params[fmt.Sprintf("values.%s", key)] = value
+		}
 		res[i] = params
 	}
 
