6585deacbd17c246a915722bd3b1ff5a8a8f2b00
OmerKahani <OmerKahani@users.noreply.github.com>
2021-04-17 23:11:08 +0300

    first impl

--- pkg/generators/metrix.go
+++ pkg/generators/metrix.go
@@ -1,23 +1,87 @@
 package generators
 
 import (
+	"reflect"
 	"time"
 
 	argoprojiov1alpha1 "github.com/argoproj-labs/applicationset/api/v1alpha1"
+	log "github.com/sirupsen/logrus"
 )
 
 var _ Generator = (*MetrixGenerator)(nil)
 
 type MetrixGenerator struct {
+	generators map[string]Generator
 }
 
-func NewMertixGenerator() Generator {
-	g := &MetrixGenerator{}
+func NewMertixGenerator(generators map[string]Generator) Generator {
+	g := &MetrixGenerator{
+		generators: generators,
+	}
 	return g
 }
 
-func (g *MetrixGenerator) GenerateParams(appSetGenerator *argoprojiov1alpha1.ApplicationSetGenerator) ([]map[string]string, error) {
-	return []map[string]string{}, nil
+func (m *MetrixGenerator) GetRelevantGenerators(requestedGenerator *argoprojiov1alpha1.ApplicationSetGenerator) []Generator {
+	var res []Generator
+
+	v := reflect.Indirect(reflect.ValueOf(requestedGenerator))
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		if !field.CanInterface() {
+			continue
+		}
+
+		if !reflect.ValueOf(field.Interface()).IsNil() {
+			res = append(res, m.generators[v.Type().Field(i).Name])
+		}
+	}
+
+	return res
+}
+
+func combineMaps(a map[string]string, b map[string]string) map[string]string {
+	res := map[string]string{}
+
+	for k, v := range a {
+		res[k] = v
+	}
+
+	for k, v := range b {
+		res[k] = v
+	}
+
+	return res
+}
+
+func (m *MetrixGenerator) GenerateParams(appSetGenerator *argoprojiov1alpha1.ApplicationSetGenerator) ([]map[string]string, error) {
+
+	allParams := [][]map[string]string{}
+
+	for _, requestedGenerator := range appSetGenerator.Metrix.Generators {
+		generator := m.GetRelevantGenerators(&requestedGenerator)[0]
+
+		params, err := generator.GenerateParams(&requestedGenerator)
+		if err != nil {
+			log.WithError(err).WithField("generator", generator).
+				Error("error generating params")
+			//if firstError == nil {
+			//	firstError = err
+			//}
+			continue
+		}
+
+		allParams = append(allParams, params)
+	}
+
+	res := []map[string]string{}
+
+	for _, a := range allParams[0] {
+		for _, b := range allParams[1] {
+			res = append(res, combineMaps(a, b))
+		}
+	}
+
+	return res, nil
 }
 
 func (g *MetrixGenerator) GetRequeueAfter(appSetGenerator *argoprojiov1alpha1.ApplicationSetGenerator) time.Duration {
--- pkg/generators/metrix_test.go
+++ pkg/generators/metrix_test.go
@@ -2,13 +2,23 @@ package generators
 
 import (
 	"testing"
+	"time"
 
 	argoprojiov1alpha1 "github.com/argoproj-labs/applicationset/api/v1alpha1"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestMetrixGenerate(t *testing.T) {
+
+	gitGeneratorSpec := argoprojiov1alpha1.ApplicationSetGenerator{
+		Git: &argoprojiov1alpha1.GitGenerator{
+			RepoURL:     "RepoURL",
+			Revision:    "Revision",
+			Directories: []argoprojiov1alpha1.GitDirectoryGeneratorItem{{Path: "*"}},
+		},
+	}
 
 	applicationSetInfo := argoprojiov1alpha1.ApplicationSet{
 		ObjectMeta: metav1.ObjectMeta{
@@ -17,33 +27,76 @@ func TestMetrixGenerate(t *testing.T) {
 		Spec: argoprojiov1alpha1.ApplicationSetSpec{
 			Generators: []argoprojiov1alpha1.ApplicationSetGenerator{
 				{
-					Metrix: []argoprojiov1alpha1.ApplicationSetGenerator{
-						{
-							Git: &argoprojiov1alpha1.GitGenerator{
-								RepoURL:     "RepoURL",
-								Revision:    "Revision",
-								Directories: []argoprojiov1alpha1.GitDirectoryGeneratorItem{{Path: "*"}},
-							},
-							List: &argoprojiov1alpha1.ListGenerator{
-								Elements: []argoprojiov1alpha1.ListGeneratorElement{
-									{
-										Cluster: "Clustre",
-										Url:     "Url",
+					Metrix: &argoprojiov1alpha1.MetrixGenerator{
+						Generators: []argoprojiov1alpha1.ApplicationSetGenerator{
+							gitGeneratorSpec,
+							{
+								List: &argoprojiov1alpha1.ListGenerator{
+									Elements: []argoprojiov1alpha1.ListGeneratorElement{
+										{
+											Cluster: "Cluster",
+											Url:     "Url",
+										},
 									},
 								},
-							}},
+							},
+						},
 					},
 				},
 			},
 		},
 	}
 
-	var metrixGenerator = NewMertixGenerator()
+	mock := &generatorMock{}
+
+	mock.On("GenerateParams", &gitGeneratorSpec).Return([]map[string]string{
+		{
+			"path":          "app1",
+			"path.basename": "app1",
+		},
+		{
+			"path":          "app2",
+			"path.basename": "app2",
+		},
+	}, nil)
+
+	var metrixGenerator = NewMertixGenerator(
+		map[string]Generator{
+			"Git":  mock,
+			"List": &ListGenerator{},
+		},
+	)
 	got, err := metrixGenerator.GenerateParams(&applicationSetInfo.Spec.Generators[0])
 
-	expected := []map[string]string{}
+	expected := []map[string]string{
+		{"path": "app1", "path.basename": "app1", "cluster": "Cluster", "url": "Url"},
+		{"path": "app2", "path.basename": "app2", "cluster": "Cluster", "url": "Url"},
+	}
 
 	assert.NoError(t, err)
 	assert.Equal(t, expected, got)
+
+}
+
+type generatorMock struct {
+	mock.Mock
+}
+
+func (g *generatorMock) GetTemplate(appSetGenerator *argoprojiov1alpha1.ApplicationSetGenerator) *argoprojiov1alpha1.ApplicationSetTemplate {
+	args := g.Called(appSetGenerator)
+
+	return args.Get(0).(*argoprojiov1alpha1.ApplicationSetTemplate)
+}
+
+func (g *generatorMock) GenerateParams(appSetGenerator *argoprojiov1alpha1.ApplicationSetGenerator) ([]map[string]string, error) {
+	args := g.Called(appSetGenerator)
+
+	return args.Get(0).([]map[string]string), args.Error(1)
+}
+
+func (g *generatorMock) GetRequeueAfter(appSetGenerator *argoprojiov1alpha1.ApplicationSetGenerator) time.Duration {
+	args := g.Called(appSetGenerator)
+
+	return args.Get(0).(time.Duration)
 
 }
